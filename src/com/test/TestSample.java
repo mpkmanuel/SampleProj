a64b6166ce0cd6c0bf1c48b366ef3ad9eb085f4c
Manuel Kumar <manuel.kumar@ad.infosys.com>
2022-12-27 02:15:47 +0530

    Testing

---
+++ src/com/test/TestSample.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author manuel.kumar
+ *
+ */
+public class TestSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TEST !!!!!");
+
+	}
+
+}
